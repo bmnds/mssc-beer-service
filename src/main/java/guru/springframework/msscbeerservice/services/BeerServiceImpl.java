d6f9d9e1d0da997c3450ed1d310b2d4c5761a95c
Bruno Mendes <b.mendes00@gmail.com>
2020-02-02 22:49:01 -0400

    Including EhCache

--- src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
@@ -1,5 +1,14 @@
 package guru.springframework.msscbeerservice.services;
 
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
 import guru.sfg.brewery.model.BeerDto;
 import guru.sfg.brewery.model.BeerPagedList;
 import guru.sfg.brewery.model.BeerStyleEnum;
@@ -8,14 +17,6 @@ import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import guru.springframework.msscbeerservice.web.controller.NotFoundException;
 import guru.springframework.msscbeerservice.web.mappers.BeerMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 /**
  * Created by jt on 2019-06-06.
@@ -23,88 +24,98 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class BeerServiceImpl implements BeerService {
-    private final BeerRepository beerRepository;
-    private final BeerMapper beerMapper;
+	private final BeerRepository beerRepository;
+	private final BeerMapper beerMapper;
 
-    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
-    @Override
-    public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
+	@Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
+	@Override
+	public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle,
+			PageRequest pageRequest, Boolean showInventoryOnHand) {
+		System.out.println("listBeers was called");
 
-        BeerPagedList beerPagedList;
-        Page<Beer> beerPage;
+		BeerPagedList beerPagedList;
+		Page<Beer> beerPage;
 
-        if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
-            //search both
-            beerPage = beerRepository.findAllByBeerNameAndBeerStyle(beerName, beerStyle, pageRequest);
-        } else if (!StringUtils.isEmpty(beerName) && StringUtils.isEmpty(beerStyle)) {
-            //search beer_service name
-            beerPage = beerRepository.findAllByBeerName(beerName, pageRequest);
-        } else if (StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
-            //search beer_service style
-            beerPage = beerRepository.findAllByBeerStyle(beerStyle, pageRequest);
-        } else {
-            beerPage = beerRepository.findAll(pageRequest);
-        }
+		if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+			// search both
+			beerPage = beerRepository.findAllByBeerNameAndBeerStyle(beerName,
+					beerStyle, pageRequest);
+		} else if (!StringUtils.isEmpty(beerName)
+				&& StringUtils.isEmpty(beerStyle)) {
+			// search beer_service name
+			beerPage = beerRepository.findAllByBeerName(beerName, pageRequest);
+		} else if (StringUtils.isEmpty(beerName)
+				&& !StringUtils.isEmpty(beerStyle)) {
+			// search beer_service style
+			beerPage = beerRepository.findAllByBeerStyle(beerStyle,
+					pageRequest);
+		} else {
+			beerPage = beerRepository.findAll(pageRequest);
+		}
 
-        if (showInventoryOnHand){
-            beerPagedList = new BeerPagedList(beerPage
-                    .getContent()
-                    .stream()
-                    .map(beerMapper::beerToBeerDtoWithInventory)
-                    .collect(Collectors.toList()),
-                    PageRequest
-                            .of(beerPage.getPageable().getPageNumber(),
-                                    beerPage.getPageable().getPageSize()),
-                    beerPage.getTotalElements());
-        } else {
-            beerPagedList = new BeerPagedList(beerPage
-                    .getContent()
-                    .stream()
-                    .map(beerMapper::beerToBeerDto)
-                    .collect(Collectors.toList()),
-                    PageRequest
-                            .of(beerPage.getPageable().getPageNumber(),
-                                    beerPage.getPageable().getPageSize()),
-                    beerPage.getTotalElements());
-        }
+		if (showInventoryOnHand) {
+			beerPagedList = new BeerPagedList(beerPage
+					.getContent()
+					.stream()
+					.map(beerMapper::beerToBeerDtoWithInventory)
+					.collect(Collectors.toList()),
+					PageRequest
+							.of(beerPage.getPageable().getPageNumber(),
+									beerPage.getPageable().getPageSize()),
+					beerPage.getTotalElements());
+		} else {
+			beerPagedList = new BeerPagedList(beerPage
+					.getContent()
+					.stream()
+					.map(beerMapper::beerToBeerDto)
+					.collect(Collectors.toList()),
+					PageRequest
+							.of(beerPage.getPageable().getPageNumber(),
+									beerPage.getPageable().getPageSize()),
+					beerPage.getTotalElements());
+		}
 
-        return beerPagedList;
-    }
+		return beerPagedList;
+	}
 
-    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
-    @Override
-    public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
-        if (showInventoryOnHand) {
-            return beerMapper.beerToBeerDtoWithInventory(
-                    beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
-            );
-        } else {
-            return beerMapper.beerToBeerDto(
-                    beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
-            );
-        }
-    }
+	@Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
+	@Override
+	public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
+		System.out.println("getById was called");
 
-    @Override
-    public BeerDto saveNewBeer(BeerDto beerDto) {
-        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
-    }
+		if (showInventoryOnHand) {
+			return beerMapper.beerToBeerDtoWithInventory(
+					beerRepository.findById(beerId)
+							.orElseThrow(NotFoundException::new));
+		} else {
+			return beerMapper.beerToBeerDto(
+					beerRepository.findById(beerId)
+							.orElseThrow(NotFoundException::new));
+		}
+	}
 
-    @Override
-    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
-        Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+	@Override
+	public BeerDto saveNewBeer(BeerDto beerDto) {
+		return beerMapper.beerToBeerDto(
+				beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+	}
 
-        beer.setBeerName(beerDto.getBeerName());
-        beer.setBeerStyle(beerDto.getBeerStyle().name());
-        beer.setPrice(beerDto.getPrice());
-        beer.setUpc(beerDto.getUpc());
+	@Override
+	public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+		Beer beer = beerRepository.findById(beerId)
+				.orElseThrow(NotFoundException::new);
 
-        return beerMapper.beerToBeerDto(beerRepository.save(beer));
-    }
+		beer.setBeerName(beerDto.getBeerName());
+		beer.setBeerStyle(beerDto.getBeerStyle().name());
+		beer.setPrice(beerDto.getPrice());
+		beer.setUpc(beerDto.getUpc());
 
-    @Cacheable(cacheNames = "beerUpcCache")
-    @Override
-    public BeerDto getByUpc(String upc) {
-        return beerMapper.beerToBeerDto(beerRepository.findByUpc(upc));
-    }
+		return beerMapper.beerToBeerDto(beerRepository.save(beer));
+	}
+
+	@Cacheable(cacheNames = "beerUpcCache")
+	@Override
+	public BeerDto getByUpc(String upc) {
+		return beerMapper.beerToBeerDto(beerRepository.findByUpc(upc));
+	}
 }
--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,17 +1,25 @@
 package guru.springframework.msscbeerservice.web.controller;
 
+import java.util.UUID;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import guru.sfg.brewery.model.BeerDto;
 import guru.sfg.brewery.model.BeerPagedList;
 import guru.sfg.brewery.model.BeerStyleEnum;
 import guru.springframework.msscbeerservice.services.BeerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 /**
  * Created by jt on 2019-05-12.
@@ -21,58 +29,67 @@ import java.util.UUID;
 @RestController
 public class BeerController {
 
-    private static final Integer DEFAULT_PAGE_NUMBER = 0;
-    private static final Integer DEFAULT_PAGE_SIZE = 25;
+	private static final Integer DEFAULT_PAGE_NUMBER = 0;
+	private static final Integer DEFAULT_PAGE_SIZE = 25;
 
-    private final BeerService beerService;
+	private final BeerService beerService;
 
-    @GetMapping(produces = { "application/json" }, path = "beer")
-    public ResponseEntity<BeerPagedList> listBeers(@RequestParam(value = "pageNumber", required = false) Integer pageNumber,
-                                                   @RequestParam(value = "pageSize", required = false) Integer pageSize,
-                                                   @RequestParam(value = "beerName", required = false) String beerName,
-                                                   @RequestParam(value = "beerStyle", required = false) BeerStyleEnum beerStyle,
-                                                   @RequestParam(value = "showInventoryOnHand", required = false) Boolean showInventoryOnHand){
+	@GetMapping(produces = {"application/json"}, path = "beers")
+	public ResponseEntity<BeerPagedList> listBeers(
+			@RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+			@RequestParam(value = "pageSize", required = false) Integer pageSize,
+			@RequestParam(value = "beerName", required = false) String beerName,
+			@RequestParam(value = "beerStyle", required = false) BeerStyleEnum beerStyle,
+			@RequestParam(value = "showInventoryOnHand", required = false) Boolean showInventoryOnHand) {
 
-        if (showInventoryOnHand == null) {
-            showInventoryOnHand = false;
-        }
+		if (showInventoryOnHand == null) {
+			showInventoryOnHand = false;
+		}
 
-        if (pageNumber == null || pageNumber < 0){
-            pageNumber = DEFAULT_PAGE_NUMBER;
-        }
+		if (pageNumber == null || pageNumber < 0) {
+			pageNumber = DEFAULT_PAGE_NUMBER;
+		}
 
-        if (pageSize == null || pageSize < 1) {
-            pageSize = DEFAULT_PAGE_SIZE;
-        }
+		if (pageSize == null || pageSize < 1) {
+			pageSize = DEFAULT_PAGE_SIZE;
+		}
 
-        BeerPagedList beerList = beerService.listBeers(beerName, beerStyle, PageRequest.of(pageNumber, pageSize), showInventoryOnHand);
+		BeerPagedList beerList = beerService.listBeers(beerName, beerStyle,
+				PageRequest.of(pageNumber, pageSize), showInventoryOnHand);
 
-        return new ResponseEntity<>(beerList, HttpStatus.OK);
-    }
+		return new ResponseEntity<>(beerList, HttpStatus.OK);
+	}
 
-    @GetMapping("beer/{beerId}")
-    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId,
-                                               @RequestParam(value = "showInventoryOnHand", required = false) Boolean showInventoryOnHand){
-        if (showInventoryOnHand == null) {
-            showInventoryOnHand = false;
-        }
+	@GetMapping("beers/{beerId}")
+	public ResponseEntity<BeerDto> getBeerById(
+			@PathVariable("beerId") UUID beerId,
+			@RequestParam(value = "showInventoryOnHand", required = false) Boolean showInventoryOnHand) {
+		if (showInventoryOnHand == null) {
+			showInventoryOnHand = false;
+		}
 
-        return new ResponseEntity<>(beerService.getById(beerId, showInventoryOnHand), HttpStatus.OK);
-    }
+		return new ResponseEntity<>(
+				beerService.getById(beerId, showInventoryOnHand),
+				HttpStatus.OK);
+	}
 
-    @GetMapping("beerUpc/{upc}")
-    public ResponseEntity<BeerDto> getBeerByUpc(@PathVariable("upc") String upc){
-        return new ResponseEntity<>(beerService.getByUpc(upc), HttpStatus.OK);
-    }
+	@GetMapping("beerUpc/{upc}")
+	public ResponseEntity<BeerDto> getBeerByUpc(
+			@PathVariable("upc") String upc) {
+		return new ResponseEntity<>(beerService.getByUpc(upc), HttpStatus.OK);
+	}
 
-    @PostMapping(path = "beer")
-    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
-        return new ResponseEntity<>(beerService.saveNewBeer(beerDto), HttpStatus.CREATED);
-    }
+	@PostMapping(path = "beers")
+	public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto) {
+		return new ResponseEntity<>(beerService.saveNewBeer(beerDto),
+				HttpStatus.CREATED);
+	}
 
-    @PutMapping("beer/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
-        return new ResponseEntity<>(beerService.updateBeer(beerId, beerDto), HttpStatus.NO_CONTENT);
-    }
+	@PutMapping("beers/{beerId}")
+	public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,
+			@RequestBody @Validated BeerDto beerDto) {
+		return new ResponseEntity<>(beerService.updateBeer(beerId, beerDto),
+				HttpStatus.NO_CONTENT);
+	}
 
 }
